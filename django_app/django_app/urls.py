6fae280df271347f1b683cf6584e75bb2b537945
root <root@Latitude-3490>
2024-01-15 01:13:00 +0500

    created views of train, eval and infernece for ddpg and ppo agents in django app

---
+++ django_app/django_app/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from .views import train_ddpg,train_ppo, evaluate_ddpg,evaluate_ppo, inference
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('ilp/', include('ilp_app.urls')),
+    path('train_ddpg/', train_ddpg, name='train_ddpg'),
+    path('evaluate_ddpg/', evaluate_ddpg, name='evaluate_ddpg'),
+    path('train_ppo/', train_ppo, name='train_ppo'),
+    path('evaluate_ppo/', evaluate_ppo, name='evaluate_ppo'),
+    path('inference/', inference, name='inference'), 
+]
+
+
