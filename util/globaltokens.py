e49e28d81b3cc3cad3cf2225f85e1578bc26ae24
idrees535 <idrees535@gmail.com>
2024-05-14 16:10:45 +0500

    gpu checks

--- util/globaltokens.py
+++ util/globaltokens.py
@@ -53,6 +53,20 @@ initial_liquidity_amount=10000000000
 
 btc_usdt_pool = UniV3Model(token0, token1,decimals_token0,decimals_token1,supply_token0,supply_token1,fee_tier,initial_pool_price,deployer,sync_pool, initial_liquidity_amount)
 
+
+token0 = "WETH"
+token1 = "BTC"
+supply_token0 = 1e18
+supply_token1 = 1e18
+decimals_token0 = 18
+decimals_token1 = 18
+fee_tier = 3000
+initial_pool_price = 20 # 20 WETH
+deployer = GOD_ACCOUNT
+sync_pool=True
+initial_liquidity_amount=10000000000
+
+btc_weth_pool = UniV3Model(token0, token1,decimals_token0,decimals_token1,supply_token0,supply_token1,fee_tier,initial_pool_price,deployer,sync_pool, initial_liquidity_amount)
 #Instantiante pool class
 #weth_usdc_pool = UniV3Model(True,10000000000,2000)
 
