c50922c5ee24f2ddd2820ac84a2c154ae6649579
idrees535 <idrees535@gmail.com>
2024-07-18 00:05:40 +0500

    jhahd

--- solcx_install.py
+++ solcx_install.py
@@ -1,7 +1,10 @@
 import solcx
 
+# Install specific version of solc
+solcx.install_solc('0.7.6')
+
 # Set the installed version as the default compiler
 solcx.set_solc_version('0.7.6')
 
 # Check if the version is set correctly
-print(solcx.get_solc_version())
+print(f"Installed and set solc version: {solcx.get_solc_version()}")
